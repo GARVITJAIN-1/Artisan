77af2dcca0027be283e1f5b951cf9b4c935ed0b8
GARVITJAIN-1 <23ucs578@lnmiit.ac.in>
2025-09-21 01:19:22 +0530

    prod3

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -10,20 +9,14 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
+      { protocol: 'https', hostname: 'placehold.co', port: '', pathname: '/**' },
+      { protocol: 'https', hostname: 'picsum.photos', port: '', pathname: '/**' },
     ],
   },
+  // cast the whole experimental object to 'any'
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  } as any,
 };
 
 export default nextConfig;
