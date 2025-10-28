5ef67f6b2c9eb418e4e0b4ef31fdffea4bce3b7b
23ucs578 <23ucs578@lnmiit.ac.in>
2025-10-28 18:35:32 +0000

    to do remove

--- src/app/artisan-assist/to-do/actions.ts
+++
@@ -1,8 +0,0 @@
-
-'use server';
-
-import { createCalendarEvent } from '@/lib/google-calendar';
-
-export const createCalendarEventAction = async (summary: string, description: string, startTime: string, endTime: string, accessToken: string) => {
-  await createCalendarEvent(summary, description, startTime, endTime, accessToken);
-};
